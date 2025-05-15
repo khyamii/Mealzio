a11a63b981aaf590185e5ba7d11e4b112be96e8d
MIDO <muhamedkhyamey@gmail.com>
2025-05-15 17:08:39 +0300

    Fixed issue

--- app/src/main/java/com/example/meals/presentation/MainActivity.kt
+++ app/src/main/java/com/example/meals/presentation/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             MaterialTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
                     val navController = rememberNavController()
